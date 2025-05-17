0157c4766f3ffbc38c5eae473edebdd2d6e9d195
Dheeraj kumar <dheeraj13a@gmail.com>
2025-05-17 15:30:52 +0530

    Update script.js

--- script.js
+++ script.js
@@ -39,7 +39,7 @@ function isEmptyInput(){
     qrText.value.length > 0 ? generateQRCode() : alert("Enter the text or URL to generate your QR code");;
 }
 
-//extract from external scriot file
+//extract from external script file
 function generateQRCode(){
     qrContainer.innerHTML = "";
     new QRCode(qrContainer, {
